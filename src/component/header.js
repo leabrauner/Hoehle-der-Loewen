b91d5571effd49fbaad95247791431aea377180a
alsecl <alexander.clauss@itgain.de>
2020-03-06 11:24:28 +0100

    Logo, Box-Sizing.

--- src/component/header.js
+++ src/component/header.js
@@ -1,11 +1,17 @@
 import React from "react";
 
+import LogoImage from '../img/logo.png';
 import HeaderImage from "../img/header.png";
 
 export default class Header extends React.Component{
     render() {
         return <div className="header">
-            <img src={HeaderImage}  />
+            <div className="logo">
+                <img src={LogoImage} />
+            </div>
+            <div className="app-header">
+                <img src={HeaderImage}  />
+            </div>
         </div>;
     }
 
