63e5b1a994f22cf29e779311a35ff67a1b76e312
alsecl <alexander.clauss@itgain.de>
2020-03-14 13:59:03 +0100

    Login aktiviert.

--- src/state/user.js
+++ src/state/user.js
@@ -1,21 +1,20 @@
 import {Login} from "../actions";
 import {http} from "../core/http";
 
-export default ({ init, on, update }) => {
+export default ({init, on, update}) => {
 
-    init({ user: null });
+    init({user: null});
 
     on(Login, (state, credentials) => {
-        // http.post('user/login', credentials).then(result => {
-        //    const user = result.data !== ''  ? result.data : null;
-        //    update({ user });
-        // });
+        http.post('user/login', credentials).then(result => {
+            const {user, error} = result.data;
 
-        if (credentials.email === 'lea' && credentials.password === 'test') {
-            update({ user: credentials });
-        } else {
-            update({ error: 'Ungültige Zugangsdaten.'})
-        }
+            if (!user && !error) {
+                update({user: null, error: 'Ungültige Zugangsdaten'});
+            } else {
+                update({user, error});
+            }
+        });
     });
 
 }
